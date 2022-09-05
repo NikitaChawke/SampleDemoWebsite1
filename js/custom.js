88b72eb25a4152987349457f0dc87be2d1926aa4
NikitaChawke <108391492+NikitaChawke@users.noreply.github.com>
2022-09-06 04:15:56 +0530

    add Header CSS & Js
    add Header CSS & Js

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+/. show and hide menu ./
+$(document).ready(function(){
+    'use strict';
+    $(window).scroll(function(){
+        'use strict';
+        if($(window).scrollTop() < 80 ){
+            $('.navbar').css({
+                'margin-top': '-100px',
+                'opacity':'0'
+            });
+            $('.navbar-default').css({
+                'background-color':'rgba(59, 59, 59, 0)'
+            });
+        }else {
+            $('.navbar').css({
+                'margin-top': '0',
+                'opacity':'1'
+            }); 
+            $('.navbar-default').css({
+                'background-color':'rgba(59, 59, 59, 0.7)',
+                'border-color':'#444'
+            }); 
+            $('.navbar-brand img').css({
+                'height':'35px',
+                'padding-top':'0px'
+            });
+            $('.navbar-nav').css({
+                'padding-top':'5px'
+            }); 
+        }
+    });
+});
